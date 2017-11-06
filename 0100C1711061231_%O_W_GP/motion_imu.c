370fbd866b09719181169cea33ecad79ee5fca57
JunhuanLi <lijunhuan0225@gmail.com>
2017-11-06 12:33:43 +0800

    0100C1711061231_%O_W_GP

---
+++ 0100C1711061231_%O_W_GP/motion_imu.c
@@ -0,0 +1,405 @@
+/*******************************************************************************
+
+  Copyright (C), 2017-2027, TOPBAND Co., Ltd.
+  File name 	: path_tracking.c
+  Author		: George 	
+  Version		: V1.0.0	  
+  Date			: 2017/07/21
+  Description	: path_tracking 
+  
+  History:		  
+				  
+	1. Date 		:
+	   Author		:
+	   Modification :
+	2. ...
+	
+*******************************************************************************/
+
+/* includes *******************************************************************/
+#include "motion_imu.h"
+/* macros *********************************************************************/
+/* static variables ***********************************************************/
+static float Tracking_2D_Angle(T_motion_tracker* obj);
+static void Tracking_2D_Line(T_motion_tracker* obj);
+/* funcitons ******************************************************************/
+/*
+void Tracking_Init(T_motion_tracker* obj,uint8_t en, T_motion_tracker_state_type state)
+{
+	obj->enable = en;
+	obj->state = state;
+	obj->error = TRACKING_ERROR_NONE;
+	//obj->sensor.mag_polarity = 1;
+	Tracking_Set_Path_Param(obj,0,0,0);
+	Tracking_Set_Angle_Param(obj,0,0,0);
+	Tracking_Set_Mag_Tracking_Param(obj,0,0,0);
+	Tracking_Set_Mag_Gotoline_Param(obj,0,0,0);
+	Tracking_Update_2D_Angle(obj,0,0,0);
+}
+*/
+
+void Motion_Set_Path_Param(T_motion_tracker* obj,float advanc)
+{
+	obj->path_imu.advancing = advanc;
+}
+
+void Motion_Set_cornerAngle_Param(T_motion_tracker* obj,float kp, float ki, float il)
+{
+	obj->path_imu.cornerAngle_pi.kp = kp;
+	obj->path_imu.cornerAngle_pi.ki = ki;
+	obj->path_imu.cornerAngle_pi.il = il;
+	obj->path_imu.rotationFinished = FALSE;
+	obj->path_imu.pre_dir_stored = FALSE;
+}
+
+void Motion_Set_lineAngle_Param(T_motion_tracker* obj,float kp, float ki, float il)
+{
+	obj->path_imu.lineAngle_pi.kp = kp;
+	obj->path_imu.lineAngle_pi.ki = ki;
+	obj->path_imu.lineAngle_pi.il = il;
+	obj->path_imu.pointReached = FALSE;
+}
+
+void Motion_Update_2D_Angle(T_motion_tracker* obj,float dir_x,float dir_y,float vel)
+{
+	Motion_Norm_2D(&dir_x,&dir_y);
+	obj->tracking														= MOTION_TRACKING_2D_ANGLE;
+	obj->path_imu.dir_x 										= dir_x;
+	obj->path_imu.dir_y 										= dir_y;	
+	obj->path_imu.dir_x_adj									= dir_x;
+	obj->path_imu.dir_y_adj									= dir_y;
+	obj->command_vel 												= vel;
+	obj->target_vel													= vel;
+//	obj->path_imu.direction_pi.integral			= 0;
+}
+
+void Motion_Update_2D_Line(T_motion_tracker* obj,float point_x, float point_y, float dir_x, float dir_y, float vel)
+{
+	Motion_Norm_2D(&dir_x,&dir_y);
+	obj->tracking  													= MOTION_TRACKING_2D_LINE;
+	obj->path_imu.point_x 									= point_x;
+	obj->path_imu.point_y 									= point_y;
+	obj->path_imu.dir_x 										= dir_x;
+	obj->path_imu.dir_y 										= dir_y;
+	obj->path_imu.dir_x_adj									= dir_x;
+	obj->path_imu.dir_y_adj									= dir_y;
+	obj->command_vel												= vel;
+	obj->target_vel 												= vel;//vel;bug 170914
+	obj->path_imu.lineAngle_pi.integral			= 0;
+	obj->path_imu.lineAngle_pi.integral			= 0;
+}
+
+void Motion_Update_2D_Arc(T_motion_tracker* obj,float point_x, float point_y, float center_x, float center_y,float vel)
+{
+	obj->tracking 													= MOTION_TRACKING_2D_ARC;
+	obj->path_imu.point_x 									= point_x;
+	obj->path_imu.point_y 									= point_y;
+	obj->path_imu.center_x 									= center_x;
+	obj->path_imu.center_y	 								= center_y;
+	obj->command_vel												= vel;
+	obj->target_vel 												= vel;
+//	obj->path_imu.position_pi.integral			= 0;
+//	obj->path_imu.direction_pi.integral			= 0;
+}
+
+void Motion_Update_2D_Point(T_motion_tracker* obj,float point_x, float point_y,float vel)
+{
+	obj->tracking 													= MOTION_TRACKING_2D_POINT;
+	obj->path_imu.point_x 									= point_x;
+	obj->path_imu.point_y 									= point_y;
+	obj->command_vel												= vel;
+	obj->target_vel													= vel;
+//	obj->path_imu.position_pi.integral			= 0;
+//	obj->path_imu.direction_pi.integral			= 0;
+}
+
+
+void Tracking_Convert_3D(T_motion_tracker* obj)
+{
+	
+}
+
+
+void Motion_Run_Tracker(T_motion_tracker* obj)
+{
+	if(obj->tracking == MOTION_TRACKING_2D_ANGLE)
+	{
+		Motion_Run_2D_Angle(obj);
+	}
+	else if(obj->tracking == MOTION_TRACKING_2D_LINE)
+	{
+		Motion_Run_2D_Line(obj);
+	}
+}
+
+
+static float Tracking_2D_Angle(T_motion_tracker* obj)
+{
+	T_motion_turn_type 									rot_dir;
+	float																err;
+	volatile float											cross_product;
+	float																dp;
+	float																pi_out;
+	float																angular_vel;
+	
+	/************************The Angle Part*****************************/
+	cross_product = (obj->sense.dir_x*obj->path_imu.dir_y_adj)-(obj->path_imu.dir_x_adj*obj->sense.dir_y);
+	dp = (obj->sense.dir_x*obj->path_imu.dir_x_adj)+(obj->sense.dir_y*obj->path_imu.dir_y_adj);
+	
+	/************ Anticlockwise positive; Clockwise negative ********************/
+	rot_dir = (cross_product < 0)?MOTION_TURN_CLOCKWISE:MOTION_TURN_COUNTERCLOCKWISE;
+	//err_angle = acosf(dot_product/sqrtf(obj->robot.dir_x*obj->robot.dir_x+obj->robot.dir_y*obj->robot.dir_y)/
+	//															sqrtf(obj->path_imu.dir_x*obj->path_imu.dir_x+obj->path_imu.dir_y*obj->path_imu.dir_y));
+
+	if(dp > 1.0f)		
+		dp = 1.0f;
+
+	err = acosf(dp);  //acosf 0 ~ pi (?)
+	
+	
+	if(rot_dir == MOTION_TURN_COUNTERCLOCKWISE)
+		err = -err;
+	
+	if(err != 0.0f)
+	{	
+		pi_out = PI_Run2(&(obj->path_imu.lineAngle_pi),err);
+	}
+	else
+	{
+		//rot_dir = MOTION_TURN_NONE;
+		pi_out = 0;
+	}
+	
+	return pi_out;
+}
+
+static void Tracking_2D_Line(T_motion_tracker* obj)
+{
+	float												err_x,err_y;
+	float												adv_x,adv_y;
+	float												dir_x,dir_y;
+	float												distance;
+	
+	/***************************The Line Part*************************************/
+	distance = (obj->path_imu.point_x-obj->sense.pos_x)*(-obj->path_imu.dir_y)
+							+(obj->path_imu.point_y-obj->sense.pos_y)*(obj->path_imu.dir_x);
+
+	err_x = -distance * obj->path_imu.dir_y;
+	err_y = distance * obj->path_imu.dir_x;
+	adv_x = obj->path_imu.advancing * obj->path_imu.dir_x;
+	adv_y = obj->path_imu.advancing * obj->path_imu.dir_y;
+	
+	dir_x = err_x + adv_x;
+	dir_y = err_y + adv_y;
+	
+	Motion_Norm_2D(&dir_x,&dir_y);
+		
+	obj->path_imu.dir_x_adj = dir_x;
+	obj->path_imu.dir_y_adj = dir_y;
+}
+
+void Motion_Run_2D_Angle(T_motion_tracker* obj)
+{
+	//Tracking_Get_Position_2D(obj);
+	obj->angular_vel = Tracking_2D_Angle(obj);
+	obj->line_vel = obj->target_vel;
+}
+
+void Motion_Run_2D_Line(T_motion_tracker* obj)
+{
+	//Tracking_Get_Position_2D(obj);
+	//Tracking_2D_Line(obj);
+	//obj->angular_vel = Tracking_2D_Angle(obj);
+	obj->line_vel = obj->target_vel;
+}
+
+float Tracking_Vect2Angle(float x, float y)
+{
+	float pi = 3.14159265;
+	volatile float angle;
+	if(x != 0)
+	{
+		angle = atan2f(y,x)/pi*180.0;
+	}
+	else
+	{
+		angle = 0;
+	}
+	return angle;
+}
+
+void rotate_angle(T_motion_tracker* obj, float angle, T_gp_side rot_dir) //angle is in degree
+{
+	float											pi_out=0.0;
+	float 										angleRotated=0.0;
+	float											dot_product=0.0;
+	float 										cross_product = 0.0;
+	float											err=0.0;
+	
+	if(obj->path_imu.pre_dir_stored == FALSE)
+	{
+		obj->path_imu.ra_pre_dir_x = obj->sense.dir_x;
+		obj->path_imu.ra_pre_dir_y = obj->sense.dir_y;
+		obj->path_imu.pre_dir_stored = TRUE;
+	}
+	
+	dot_product = (obj->sense.dir_x * obj->path_imu.ra_pre_dir_x)+(obj->sense.dir_y * obj->path_imu.ra_pre_dir_y);
+	
+	if(dot_product > 1.0)
+		dot_product = 1.0;
+	
+	angleRotated = acosf(dot_product)*57.3;
+
+	 
+	err = angle - angleRotated;
+	if(fabsf(err)<=3) //in degree
+	{
+		obj->path_imu.pre_dir_stored = FALSE;
+		obj->path_imu.rotationFinished = TRUE;
+	}
+	else
+	{
+		if(rot_dir == LEFT_STEERING)
+		{
+			set_velocity(obj, 0, 0.5);
+		}	
+		else
+		{
+			set_velocity(obj, 0, -0.5);
+		}	
+		obj->path_imu.rotationFinished = FALSE;
+	}
+}
+
+void rotate_vector(T_motion_tracker* obj, float vec_x, float vec_y, T_gp_side rot_dir, float linear_vel) //angle is in degree
+{
+	//转角度测试相关参数
+
+	float											pi_out=0.0;
+	float											dot_product=0.0;
+	float											cross_product=0.0;
+	float 										angleError=0.0;
+	Motion_Norm_2D(&vec_x, &vec_y);
+	dot_product = (obj->sense.dir_x*vec_x)+(obj->sense.dir_y*vec_y);
+	//cross_product = (obj->sense.dir_x * obj->path_imu.pre_dir_y) - (obj->sense.dir_y * obj->path_imu.pre_dir_x);
+	
+	if(dot_product > 1.0)
+		dot_product = 1.0;
+	
+	if(dot_product < -1.0)
+		dot_product = -1.0;
+	
+ 	angleError = acosf(dot_product)*57.3;
+	
+	if(fabsf(angleError)<=10) //in degree
+	{
+		obj->path_imu.cornerAngle_pi.integral = 0;
+		obj->path_imu.rotationFinished = TRUE;
+	}
+	else
+	{
+		pi_out = PI_Run2(&(obj->path_imu.cornerAngle_pi),-angleError);
+
+		if(rot_dir == RIGHT_STEERING)
+		{
+				obj->angular_vel = pi_out;
+		}	
+		else
+		{
+				obj->angular_vel = -pi_out;
+		}	
+		obj->line_vel = linear_vel;
+		obj->path_imu.rotationFinished = FALSE;
+	}
+}
+
+void track_vector(T_motion_tracker* obj, float vec_x, float vec_y, float HEADING_CTRL_LINEAR_VEL)
+{
+	
+	//走直线测试程序相关参数
+	float dp = 0.0;
+	float cp = 0.0;
+	float error = 0.0;
+	float dist_sq = 0.0;
+	T_motion_turn_type 	rot_dir;
+	
+	//Motion_Norm_2D(&vec_x,&vec_y);
+	dp = vec_x*obj->sense.dir_x + vec_y*obj->sense.dir_y;
+	cp = vec_x*obj->sense.dir_y - vec_y*obj->sense.dir_x;
+	
+	if(dp > 1.0f)
+		dp = 1.0f;
+
+	error = acosf(dp)*57.4;
+	rot_dir = (cp<0)?MOTION_TURN_CLOCKWISE:MOTION_TURN_COUNTERCLOCKWISE;
+	
+	if(rot_dir == MOTION_TURN_COUNTERCLOCKWISE)
+		error = -error;
+	if(error != 0.0f)
+	{		
+		obj->angular_vel = PI_Run2(&(obj->path_imu.lineAngle_pi),-error);
+		obj->line_vel = HEADING_CTRL_LINEAR_VEL;
+	}
+	obj->path_imu.pointReached = FALSE;	
+}
+
+void track_point(T_motion_tracker* obj, float target_x, float target_y)
+{
+	//走直线点位控制测试程序相关参数
+	float vec_x = 0.0;
+	float vec_y = 0.0;
+	float dp = 0.0;
+	float cp = 0.0;
+	float error = 0.0;
+	float dist = 0.0;
+	float dist_sq = 0.0;
+	T_motion_turn_type 	rot_dir;
+	
+	vec_x = target_x- obj->sense.pos_x;
+	vec_y = target_y - obj->sense.pos_y;
+	dist_sq = vec_x*vec_x + vec_y*vec_y;
+	dist = sqrtf(dist_sq);
+	
+	if(dist < 0.2)//m
+	{
+		//rt_kprintf("dist=%d:",(int)(dist*1000));
+		obj->path_imu.pointReached = TRUE;
+		//rt_kprintf("%d th point reached! \n", cnt111);
+	}
+	else
+	{
+		Motion_Norm_2D(&vec_x,&vec_y);
+		dp = vec_x*obj->sense.dir_x + vec_y*obj->sense.dir_y;
+		cp = vec_x*obj->sense.dir_y - vec_y*obj->sense.dir_x;
+		
+		if(dp > 1.0f)
+			dp = 1.0f;
+
+		error = acosf(dp)*57.3;
+		rot_dir = (cp<0)?MOTION_TURN_CLOCKWISE:MOTION_TURN_COUNTERCLOCKWISE;
+		
+		if(rot_dir == MOTION_TURN_COUNTERCLOCKWISE)
+			error = -error;
+		if(error != 0.0f)
+		{		
+			obj->angular_vel = PI_Run2(&(obj->path_imu.lineAngle_pi),-error);
+			obj->line_vel = 0.1;
+		}
+		obj->path_imu.pointReached = FALSE;
+	}
+	
+}
+void set_velocity(T_motion_tracker* obj, float linear_vel, float angular_vel)
+{
+	obj->line_vel = linear_vel;
+	obj->angular_vel = angular_vel;
+}
+
+void set_velocity_hard(T_motion_tracker* obj, float linear_vel, float angular_vel)
+{
+	obj->line_vel = linear_vel;
+	obj->angular_vel = angular_vel;
+	
+	Motion_Process_Motor_Speed();
+	update_motor_control();
+}
---
+++ 0100C1711061231_%O_W_GP/thread_motion.c
@@ -0,0 +1,469 @@
+
+/*************************************************
+  Copyright (C), 2017-2027, TOPBAND Co., Ltd.
+  File name:		thread_motion.c
+  Author:			Raymond
+  Date:				2017.7.7
+  Version:        
+  Description:    // 用于详细说明此程序文件完成的主要功能，与其他模块
+                  // 或函数的接口，输出值、取值范围、含义及参数间的控
+                  // 制、顺序、独立或依赖等关系
+  History:        // 修改历史记录列表，每条修改记录应包括修改日期、修改
+                  // 者及修改内容简述  
+    1. Date:			2017.9.3
+       Author:		Nate
+       Modification:zigzag for map planner test
+    2. ...
+*************************************************/
+
+#include "stm32f4xx.h"
+#include <rtthread.h>
+//#include <string.h>
+#include <stdio.h>
+#include "mower_common.h"
+#include "motion_control.h"
+#include "global.h"
+#include "motor_control.h"
+#include "motion_mag_line.h"
+
+#include "usart_driver.h"
+#include "lcd12864_io_spi.h"
+#include "bumper_front.h"
+
+#include "global_planner.h"
+
+/*********Temp Functions******************/
+#define BACK_LINEAR_VEL -0.1
+extern T_bool g_end_of_to_pose;
+unsigned short g_counter = 0;
+
+struct rt_messagequeue mt_mq; 
+char mt_msg_pool[64];
+
+/*在分配堆栈空间时，必须要对其*/
+ALIGN(RT_ALIGN_SIZE)
+char thread_motion_stack[4096];
+
+struct rt_thread thread_motion;
+extern T_frontBumper g_Bumper;
+
+extern T_gp_trigger g_trigger;
+extern T_gp_decision g_decision;
+extern T_bool g_bumped;
+extern T_motion motion;
+//extern g_gp_decision g_decision.action;
+//extern T_gp_params g_action_params;
+extern rt_uint8_t start_move;
+/************************************/
+
+void mower_motion_thread(void* parameter)
+{	
+	rt_uint32_t recved;
+
+	mag_sensor_initial();
+	
+	rt_thread_delay(1000); //need be removed later
+	Motion_Init(&motion,1);
+	
+	motion.motion_state = MOTION_STATE_OUT_STATION;
+	motion.zigzag.state = T_MOTION_ZIGZAG_STATE_LINE;
+	
+	g_trigger = INIT_PROC;
+	
+	rt_kprintf("\r\n Thread Motion Initializing...");
+	
+	rt_kprintf("\r\n g_att_valid == %d", g_att_valid);
+	while(g_att_valid < 2)
+	{
+		
+		hw_ms_delay(1);
+	}
+	rt_kprintf("\r\n g_att_valid ok...");hw_ms_delay(1);
+	
+	rt_kprintf("\r\n wait for button press...");
+	while(1 == start_move)
+	{
+		
+	}
+	rt_kprintf("\r\n button pressed, motion control start...");
+	
+	get_decision(&g_trigger, &g_decision);
+	action_params_print();
+  
+	
+    /*test block*/
+//    g_decision.action = ONTO_WIRE;
+//    g_decision.params.onto_wire.fin_vec[0] = 1.0;
+//    g_decision.params.onto_wire.fin_vec[1] = 0.0;
+    
+//	while(1)
+//	{
+//		rt_event_recv(&sys_event, SYS_EVN_MOTION, RT_EVENT_FLAG_AND|RT_EVENT_FLAG_CLEAR, RT_WAITING_FOREVER, &recved);
+//		Motion_Get_Position_2D(&motion.tracker.sense);
+//		Motion_Get_Sensor(&motion.tracker.sense);
+//		
+//		if(circle_cali == 2)
+//		{
+//			Motion_Mag_Line_Test(&motion.tracker);
+//		}
+//		//mower_motion_circle(&motion, 0.2, 0.1);
+//		else if(circle_cali == 1)
+//		{
+//			motion.tracker.line_vel = 0.08;
+//			motion.tracker.angular_vel = 0.2;
+//		}
+//		else if(circle_cali == 3)
+//		{
+//			rt_kprintf("\r\n motion start!!!!!!!!!!!!!!11"); 
+//			motion.tracker.line_vel = 0.08;
+//			motion.tracker.angular_vel = 0.0;
+//		}
+//		//setVelocity(&motion.tracker, 0.0, 0.0);
+//		Motion_Process_Motor_Speed();
+//		update_motor_control();
+//		
+//	}
+
+
+    /*endof test block*/ 
+
+    /*onto wire test block*/
+//    g_decision.action = ONTO_WIRE;
+//    g_decision.params.onto_wire.fin_vec[0] = 1.0;
+//    g_decision.params.onto_wire.fin_vec[1] = 0.0;
+    
+//	while(1)
+//	{
+//		rt_event_recv(&sys_event, SYS_EVN_MOTION, RT_EVENT_FLAG_AND|RT_EVENT_FLAG_CLEAR, RT_WAITING_FOREVER, &recved);
+//		Motion_Get_Position_2D(&motion.tracker.sense);
+//		Motion_Get_Sensor(&motion.tracker.sense);
+
+
+//    if(motion.motion_state == MOTION_STATE_MAGLINE)
+//		{
+//			motion.tracker.target_vel = 0.4;
+//			motion.tracker.line_vel = 0.4;
+//			
+//			Motion_Mag_Line_Test(&motion.tracker);
+//			
+//			record_track(); // global_planner_record 
+//    }
+//    else
+//    {
+//       onto_wire(LEFT_STEERING, 0.1);
+//       rt_kprintf("INSIDE ONTO WIRE\n");
+//    }
+//		//set_velocity(&motion.tracker, 0.0, 0.0);
+//		Motion_Process_Motor_Speed();
+//		update_motor_control();
+//		
+//	}
+
+    /*endof onto wire test block*/ 
+
+		/*back to base test*/
+//		T_bool bumper_flag = FALSE;
+//		while(1)
+//		{
+//		rt_event_recv(&sys_event, SYS_EVN_MOTION, RT_EVENT_FLAG_AND|RT_EVENT_FLAG_CLEAR, RT_WAITING_FOREVER, &recved);
+
+//			if(g_att_valid == 2)
+//			{	
+//			
+//				Motion_Get_Position_2D(&motion.tracker.sense);
+//				Motion_Get_Sensor(&motion.tracker.sense);
+//				
+//				get_front_bumper_info(&g_Bumper);
+//				if( !bumper_flag )
+//				{
+//					Motion_Mag_Line_Test(&motion.tracker);
+//					if( g_Bumper.left == 0)
+//					{
+//						set_velocity(&motion.tracker, 0, 0);
+//						bumper_flag = TRUE;
+//					}
+//				
+//				}
+//				Motion_Process_Motor_Speed();
+//				update_motor_control();
+//				
+//				update_map_nav();
+//			}
+//		}
+/*endof back to base test*/
+
+/*mag sensor test*/
+//	while (1)
+//	{
+//		rt_event_recv(&sys_event, SYS_EVN_MOTION, RT_EVENT_FLAG_AND|RT_EVENT_FLAG_CLEAR, RT_WAITING_FOREVER, &recved);
+
+//		if(g_att_valid == 2)
+//		{	
+//			mag_sensor_update();
+//		}
+//	}
+
+/*endof mag sensor test*/
+
+/*sonar test*/
+//while(1)
+//{
+//	rt_event_recv(&sys_event, SYS_EVN_MOTION, RT_EVENT_FLAG_AND|RT_EVENT_FLAG_CLEAR, RT_WAITING_FOREVER, &recved);
+
+//	if(g_att_valid == 2)
+//	{	
+//	
+//		Motion_Get_Position_2D(&motion.tracker.sense);
+//		Motion_Get_Sensor(&motion.tracker.sense);
+//		
+////			rt_kprintf("left_dist: %d, right_dist: %d \n", 
+////								(int)(1000*motion.tracker.sense.sonar_l), (int)(1000*motion.tracker.sense.sonar_r) );
+//		
+//		Motion_Process_Motor_Speed();
+//		update_motor_control();
+//		
+//		update_map_nav();
+//	}
+//}
+/*endof sonar test*/
+
+/*rotate angle test*/
+//while(1)
+//{
+//	rt_event_recv(&sys_event, SYS_EVN_MOTION, RT_EVENT_FLAG_AND|RT_EVENT_FLAG_CLEAR, RT_WAITING_FOREVER, &recved);
+
+//	if(g_att_valid == 2)
+//	{	
+//	
+//		Motion_Get_Position_2D(&motion.tracker.sense);
+//		Motion_Get_Sensor(&motion.tracker.sense);
+//		
+//		if(!motion.tracker.path_imu.rotationFinished)
+//			rotate_angle(&motion.tracker, 90, LEFT_STEERING);
+//		else
+//		{
+//			motion.tracker.line_vel = 0.1;
+//			motion.tracker.angular_vel = 0;
+//			g_counter++;
+//			if(g_counter>=100)
+//			{
+//				g_counter = 0;
+//				motion.tracker.path_imu.rotationFinished = FALSE;
+//			}
+//		}
+//		
+//		
+//		Motion_Process_Motor_Speed();
+//		update_motor_control();
+//		
+//		update_map_nav();
+//	}
+//}
+/*endof rotate angle test*/
+
+		
+/*deceleration and OA test*/
+//extern T_bool g_obstacle_sensed;
+//extern T_bool g_end_of_arc_reached;
+//motion.zigzag.state = T_MOTION_ZIGZAG_STATE_IDLE;
+//while(1)
+//{
+//	rt_event_recv(&sys_event, SYS_EVN_MOTION, RT_EVENT_FLAG_AND|RT_EVENT_FLAG_CLEAR, RT_WAITING_FOREVER, &recved);
+
+//	if(g_att_valid == 2)
+//	{	
+//	
+//		Motion_Get_Position_2D(&motion.tracker.sense);
+//		Motion_Get_Sensor(&motion.tracker.sense);
+//		
+////		deceleration test
+////			if(!g_obstacle_sensed)
+////			{
+////				motion.tracker.angular_vel = 0;
+////				motion.tracker.line_vel = tune_linear_vel(&motion);
+////			}		
+////			else
+////				set_velocity(&motion.tracker,0, 0);
+////		
+//		
+////		OA test
+////			if(g_obstacle_sensed)
+////			{
+////				obstacle_avoidance(0.1, 0.25);
+////				if(g_end_of_arc_reached)
+////      //if(motion.zigzag.state == T_MOTION_ZIGZAG_STATE_LINE)    
+////					set_velocity(&motion.tracker, 0, 0);
+////			}
+////			
+//		
+//		Motion_Process_Motor_Speed();
+//		update_motor_control();
+//		
+//		update_map_nav();
+//	}
+//}
+/*endof deceleration and OA test*/
+	while (1)
+	{
+		rt_event_recv(&sys_event, SYS_EVN_MOTION, RT_EVENT_FLAG_AND|RT_EVENT_FLAG_CLEAR, RT_WAITING_FOREVER, &recved);
+
+		if(g_att_valid == 2)
+		{	
+			Motion_Get_Position_2D(&motion.tracker.sense);
+			Motion_Get_Sensor(&motion.tracker.sense);
+
+			if(motion.motion_state == MOTION_STATE_OUT_STATION)
+				out_station(BACK_LINEAR_VEL);
+//			{
+//				motion.tracker.line_vel = BACK_LINEAR_VEL;
+//				g_counter ++;						//when outing station
+//				if(g_counter > 150)
+//				{
+//					if(!motion.tracker.path_imu.rotationFinished)
+//						rotate_vector(&motion.tracker, -1, 0, RIGHT_STEERING, 0);
+//					else
+//					{
+//						motion.motion_state = MOTION_STATE_MAGLINE;
+//						motion.tracker.path_imu.rotationFinished = FALSE;
+//						g_counter = 0;
+//					}
+//				}
+//			}
+			else if(motion.motion_state == MOTION_STATE_MAGLINE)
+			{
+				Motion_Mag_Line_Test(&motion.tracker);
+				
+				record_track(); // global_planner_record 
+				
+				processing_bump();
+				
+				if(g_counter>100 && g_bumped )
+				{
+					if(!motion.tracker.path_imu.rotationFinished)
+					{
+						rotate_vector(&motion.tracker, g_decision.params.to_pose.pos[0]-motion.tracker.sense.pos_x, 
+													   g_decision.params.to_pose.pos[1]-motion.tracker.sense.pos_y, 
+													   LEFT_STEERING, 0);
+					}
+					else
+					{
+            motion.tracker.path_imu.rotationFinished = FALSE;
+						g_counter =0 ;
+						g_Bumper.bumper_state = 0;
+						g_bumped = FALSE;
+						
+						motion.motion_state = MOTION_STATE_P2P;
+					}
+				}
+//				get_front_bumper_info(&g_Bumper);
+//				if(g_Bumper.left == 0)
+//				{
+//					g_trigger = RIGID_TOUCHED;
+//					set_velocity(&motion.tracker, 0, 0);
+//					Motion_Process_Motor_Speed();
+//					update_motor_control();
+//					get_decision(&g_trigger, &g_decision);
+//					action_params_print();
+//				}
+//				
+//				if(g_trigger == RIGID_TOUCHED)
+//				{
+//					motion.tracker.line_vel = -0.1;
+//					g_counter++;
+//					if(g_counter>100)
+//					{
+//						if(!motion.tracker.path_imu.rotationFinished)
+//						{
+//							rotate_vector(&motion.tracker, g_decision.params.to_pose.pos[0]-motion.tracker.sense.pos_x, 
+//                                                           g_decision.params.to_pose.pos[1]-motion.tracker.sense.pos_y, 
+//                                                           LEFT_STEERING, 0);
+//						}
+//						else
+//						{
+//							motion.tracker.path_imu.rotationFinished = FALSE;
+//							g_counter =0 ;
+//							motion.motion_state = MOTION_STATE_P2P;
+//						}
+//					}
+//				}
+					
+			}
+			else if(motion.motion_state == MOTION_STATE_P2P)
+			{
+				if(!motion.tracker.path_imu.pointReached)
+				{
+					track_point(&motion.tracker, g_decision.params.to_pose.pos[0], g_decision.params.to_pose.pos[1]);
+				}
+				else
+				{
+					motion.tracker.path_imu.pointReached = FALSE;
+					
+					g_trigger = POS_REACHED;
+					get_decision(&g_trigger, &g_decision);
+					action_params_print();
+
+					if(g_decision.action == DIR_DRIVE)
+					{
+						g_end_of_to_pose = TRUE;
+						motion.motion_state = MOTION_STATE_ZIGZAG;
+						motion.zigzag.state = T_MOTION_ZIGZAG_STATE_LINE; //20171104 for ljh(remote)
+					}
+					else if(g_decision.action == TO_POSE)
+					{}
+					else
+						rt_kprintf("Action without assigned reactions[1].");
+				}
+			}
+      else if( motion.motion_state == MOTION_STATE_ZIGZAG )
+			{
+				motion_cover();
+			}
+			
+			//Update Motor Command
+			Motion_Process_Motor_Speed();
+			update_motor_control();
+			
+			update_map_nav();
+		}
+	#ifdef MOTION_CHECK_THREAD_TIME
+		temp=(float)get_time_diff3(&MOTION_thread_time_check)*1e-3;
+		rt_kprintf("\r\n..>>>:MOTION_thread_time_check_in = %d\r\n",(rt_uint32_t)temp);
+	#endif /* end of MOTION_CHECK_THREAD_TIME*/		
+	}
+}
+
+
+static void mower_motion_circle(T_motion* motion,float line_speed, float angle_speed)
+{
+	float current_angle = 0;
+	float tx = 1.0f;
+	float ty = 0.0f;
+	rt_uint32_t recved;
+	uint8_t buf[100] = "hello!!!!!!!!!\n\r";
+	while(1)
+	{
+		volatile float sin = sinf(current_angle);
+		volatile float cos = cosf(current_angle);
+		volatile float x = tx;
+		volatile float y = ty;
+		volatile float tx = x*cos-y*sin;
+		volatile float ty = x*sin+y*cos;
+		
+		rt_event_recv(&sys_event, SYS_EVN_MOTION, RT_EVENT_FLAG_AND|RT_EVENT_FLAG_CLEAR, RT_WAITING_FOREVER, &recved);
+		
+		//rt_debug(buf,16);
+		Motion_Get_Position_2D(&motion->tracker.sense);
+		Motion_Get_Sensor(&motion->tracker.sense);
+		Motion_Update_2D_Angle(&motion->tracker,tx,ty,line_speed);
+		Motion_Run_Tracker(&motion->tracker);
+		Motion_Process_Motor_Speed();
+		
+		current_angle -= angle_speed;
+		if(current_angle < 0)
+		{
+			current_angle += 2*3.1415926;
+		}
+		
+		update_motor_control();
+	}
+}
